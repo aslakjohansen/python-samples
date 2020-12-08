703b5228101f757d67f5e508407a090bbe7450d8
Aslak Johansen <aslakj@gmail.com>
2020-12-08 02:10:21 +0100

    Added script to convert a latex expression into an SVG file without page

---
+++ topics/cairo/latex-include/render-latex.py
@@ -0,0 +1,76 @@
+#!/usr/bin/env python3
+
+from subprocess import Popen, STDOUT, PIPE
+from os import linesep
+
+prefix = 'prefix'
+
+latex = '''
+$
+f(n) = \sqrt{n^2}
+$
+'''
+#$
+#f(n) =
+#\\begin{cases}
+#  n/2 , & \\text{if }n\\text{ is even} \\\\
+#  3n+1, & \\text{if }n\\text{ is odd}
+#\end{cases}
+#$
+
+
+# paths
+tex_filename      = '%s.tex' % prefix
+pdf_filename      = '%s.pdf' % prefix # TODO: Fails if base is not shared with tex_filename
+pdf_crop_filename = '%s_crop.pdf' % prefix
+svg_crop_filename = '%s_crop.svg' % prefix
+output_filename   = '%s.svg' % prefix
+
+#####################################################################
+############################################################# helpers
+
+def system (command, err=STDOUT, out=PIPE):
+    print(command)
+    p = Popen(command, shell=True, stderr=err, stdout=out)
+    output = p.communicate()[0]
+    return output.decode('utf-8') 
+
+def write_file (filename, lines):
+    with open(filename, 'w') as fo:
+        fo.writelines(lines)
+
+def generate_latex_file (filename, contents):
+    lines = [
+        '\\documentclass{article}',
+        '\\usepackage[utf8]{inputenc}',
+        '\\usepackage{amsmath,amssymb,amsfonts,amsthm}',
+        '\\begin{document}',
+        '\\thispagestyle{empty}',
+        contents,
+        '\\end{document}',
+    ]
+    write_file(filename, map(lambda line: line+linesep, lines))
+
+def latex2pdf (ifilename, ofilename):
+    system('pdflatex %s' % ifilename)
+    tfilename = '.'.join(ifilename.split('.')[:-1])+'.pdf'
+    system('cp %s %s' % (tfilename, ofilename))
+
+def crop_pdf (ifilename, ofilename):
+    system('pdfcrop %s %s' % (ifilename, ofilename))
+
+def pdf2svg (ifilename, ofilename):
+    system('pdf2svg %s %s' % (ifilename, ofilename))
+
+def embed_svg (ifilename, ofilename):
+    pass
+
+#####################################################################
+################################################################ main
+
+generate_latex_file(tex_filename, latex)
+latex2pdf(tex_filename, pdf_filename)
+crop_pdf(pdf_filename, pdf_crop_filename)
+pdf2svg(pdf_crop_filename, svg_crop_filename)
+embed_svg(svg_crop_filename, output_filename)
+
