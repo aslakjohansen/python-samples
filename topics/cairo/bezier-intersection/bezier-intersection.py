34a888a9d6cb9215318f6a551dfb55a5d5d7a64a
Aslak Johansen <aslakj@gmail.com>
2020-12-26 12:52:05 +0100

    Updated sizes for extra pretty

--- topics/cairo/bezier-intersection/bezier-intersection.py
+++ topics/cairo/bezier-intersection/bezier-intersection.py
@@ -19,7 +19,7 @@ bezier = [(100,540), (400,640), (1520,440), (1820,540)]
 def draw_control_points (ctx, bezier):
     for point in bezier:
         ctx.save()
-        ctx.arc(point[0], point[1], 2, 0, 2*pi)
+        ctx.arc(point[0], point[1], 4, 0, 2*pi)
         ctx.set_source_rgb(0, 1, 0)
         ctx.fill()
         ctx.restore()
@@ -32,7 +32,7 @@ def draw_bezier (ctx, bezier):
     p2 = bezier[2]
     p3 = bezier[3]
     ctx.curve_to(p1[0], p1[1], p2[0], p2[1], p3[0], p3[1])
-    ctx.set_line_width(1)
+    ctx.set_line_width(2)
     ctx.set_source_rgb(1, 0, 0)
     ctx.stroke()
     ctx.restore()
